7b8d633cf9735581655a739ded0567dfd634b063
ankit8801 <ankitjadhav7865@gamil.com>
2025-10-02 20:45:49 +0530

    Initial commit - demo website

---
+++ script.js
@@ -0,0 +1,14 @@
+// Scroll reveal
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 100;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+reveal();
